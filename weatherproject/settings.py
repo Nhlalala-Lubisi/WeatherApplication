710a07bf115ef608fa7731982b95eac802dff50c
Nhlalala Lubisi <lubisinhlaks685@gmail.com>
2025-09-02 22:21:27 +0200

    Update settings.py

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -132,7 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://weatherapplication-production-89a6.up.railway.app/',
+    'https://weatherapplication-production-89a6.up.railway.app',
     
 ]
+
 
